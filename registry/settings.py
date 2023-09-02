79b114383230ad2bb5466dbaf07f0e8ce9412f7a
Robert Kuykendall <robert@rkuykendall.com>
2023-09-02 09:40:49 -0500

    Try to fix build by adding whitenoise

--- registry/settings.py
+++ registry/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+if not os.environ.get("PRODUCTION"):
+    load_dotenv()
+
+DEBUG = os.environ.get("DEBUG")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-he815)=4d#53wyl6(x1h#p9k6oi)899x%+4dy!+rvv2qhxmyg%"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "baby-zelada-registry-2216bd384938.herokuapp.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -39,15 +49,19 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+MIDDLEWARE = (
+    [
+        "django.middleware.security.SecurityMiddleware",
+    ]
+    + (["whitenoise.middleware.WhiteNoiseMiddleware"] if not DEBUG else [])
+    + [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+])
 
 ROOT_URLCONF = "registry.urls"
 
@@ -79,6 +93,12 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+if db_from_env:
+    DATABASES["default"].update(db_from_env)
 
 
 # Password validation
