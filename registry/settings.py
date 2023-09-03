58aa7a01d4ca0954bd3c8220453289e6f6d75c98
Robert Kuykendall <robert@rkuykendall.com>
2023-09-03 09:12:54 -0500

    Try to fix build by adding STORAGES

--- registry/settings.py
+++ registry/settings.py
@@ -137,6 +137,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
