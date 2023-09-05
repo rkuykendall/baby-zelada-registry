87b0f3e76fa4cc859d784dc47d8343b8852b9034
Robert Kuykendall <robert@rkuykendall.com>
2023-09-05 09:02:04 -0500

    Add photos

--- items/admin.py
+++ items/admin.py
@@ -8,7 +8,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "category", "quantity_desired")
+    list_display = ("name", "price", "category", "quantity_desired", "photo")
     search_fields = ("name", "description")
     list_filter = ("category",)
 
--- items/models.py
+++ items/models.py
@@ -13,9 +13,8 @@ class Category(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    link = models.URLField(
-        blank=True, null=True
-    )  # Link to where the item can be purchased.
+    link = models.URLField(blank=True, null=True)
+    photo = models.URLField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     quantity_desired = models.PositiveIntegerField(default=1)
