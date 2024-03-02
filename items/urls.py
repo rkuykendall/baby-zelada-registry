feb867798e415dc5131ea8053be57e2918eee15d
Robert Kuykendall <robert@rkuykendall.com>
2024-03-02 10:39:27 -0600

    Add CSV exports

--- items/urls.py
+++ items/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.item_list, name="item_list"),
     path("gift/", views.create_gift, name="create_gift"),
+    path("export-items/", views.export_items_csv, name="export_items_csv"),
+    path("export-gifts/", views.export_gifts_csv, name="export_gifts_csv"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,9 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.db.models import Sum, Prefetch
 from django.db.models.functions import Coalesce
-from .models import Item, Category
+from .models import Item, Category, Gift
 from .forms import GiftForm
 
 
@@ -24,6 +26,55 @@ def item_list(request):
 
     context = {"categories": categories, "user_authenticated": user_authenticated}
     return render(request, "items_list.html", context)
+
+
+def export_items_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="items.csv"'},
+    )
+
+    writer = csv.writer(response)
+
+    # Write the headers to the CSV file.
+    writer.writerow(['Category Name', 'Item Name', 'Number of Gifts', 'Quantity Desired', 'Gifts Remaining'])
+
+    # Query the database for items
+    categories = Category.objects.all().prefetch_related(
+        Prefetch(
+            "item_set",
+            queryset=Item.objects.annotate(
+                num_gifts=Coalesce(Sum("gift__quantity"), 0)
+            ),
+        )
+    ).filter(item__isnull=False).distinct()
+
+    # Write data rows
+    for category in categories:
+        for item in category.item_set.all():
+            gifts_remaining = item.quantity_desired - item.num_gifts
+            writer.writerow([category.name, item.name, item.num_gifts, item.quantity_desired, gifts_remaining])
+
+    return response
+
+
+def export_gifts_csv(request):
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="gifts.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['Item Name', 'Quantity', 'Given By'])
+
+    # Assuming 'Gift' model has fields like 'quantity', 'given_by', and a foreign key to 'Item'
+    gifts = Gift.objects.select_related('item').all()
+
+    for gift in gifts:
+        writer.writerow([gift.item.name, gift.quantity, gift.giver_name])
+
+    return response
 
 
 def create_gift(request):
